8216d0ea43d05bb6557b7c8ebefc4c33b9748caa
北出　紘章 <01041928@CA7206.local>
2021-04-26 18:32:19 +0900

    コメント追記

--- advanced/client/js/__test__/test.js
+++ advanced/client/js/__test__/test.js
@@ -4,4 +4,5 @@ test('HTMLエスケープのテスト', () => {
   const arg = '<script>alert("XSS");</script>';
   ans = Views.myEscape(arg);
   console.log(ans);
+  // Assert: &lt;script&gt;alert(&quot;XSS&quot;)&lt;/script&gt;
 });
